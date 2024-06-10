43e2e47bc50fce23784b80b391f0caf4025f4c09
Stephanie You <stephanie@dolthub.com>
2024-06-10 16:28:56 -0700

    add pagination to log table

--- app/(home)/log/page.tsx
+++ app/(home)/log/page.tsx
@@ -1,10 +1,18 @@
-import Table from '../../ui/table';
+import Table from '../../ui/log/table';
+import Pagination from '../../ui/log/pagination';
 
 export default function Page() {
+    const totalPages = 1;
+    
     return (
       <div>
-        <div className="text-4xl">transactions</div>
-        <Table />
+        <div>
+          <div className="text-4xl">transactions</div>
+          <Table />
+        </div>
+        <div className="mt-5 flex w-full justify-center">
+          <Pagination totalPages={totalPages} />
+        </div>
       </div>
     );
-  }
+}
