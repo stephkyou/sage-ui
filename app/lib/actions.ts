0318b42244044d85280f669dffbd6e412e76a2fc
Stephanie You <stephanie@dolthub.com>
2024-06-27 13:11:58 -0700

    add form adds data to database

---
+++ app/lib/actions.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { z } from 'zod';
+import moment from 'moment';
+
+type FormState = {
+  message: string;
+}
+
+const FormSchema = z.object({
+  date: z.string(),
+  location: z.string(),
+  description: z.string(),
+  amount: z.coerce.number(),
+});
+
+export async function addExpense(prevState: FormState, formData: FormData) {
+  const { date, location, description, amount } = FormSchema.parse({
+    date: formData.get("date"),
+    location: formData.get("location"),
+    description: formData.get("description"),
+    amount: formData.get("amount"),
+  })
+
+  const formattedDate = moment(date).format('YYYY-MM-DD');
+
+  const req = "http://127.0.0.1:8080/add?date="+formattedDate+"&location="+location+"&description="+description+"&amount="+amount
+  await fetch(req, {
+    method: "POST"
+  })
+
+  return {
+    message: "Form data processed",
+  } 
+  }
